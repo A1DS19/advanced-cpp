c66f41fd235d8a126f5e01a68059ff1c960a5470
Jose Padilla <59151555+A1DS19@users.noreply.github.com>
2025-05-14 14:36:57 -0700

    use references instead of values

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,7 @@
 #include <stdlib.h>
 
-#include <iostream>
-
 #include "algorithm.hpp"
+#include "common.hpp"
 #include "functional.hpp"
 #include "strings.hpp"
 
@@ -10,7 +9,7 @@ int main() {
   Container cn = {2, 3, 4, 5, 6, 7, 8, 9};
   auto result = keepIf(isEven, cn);
 
-  for (const auto c : result) {
+  for (const auto &c : result) {
     std::cout << c << std::endl;
   }
 
@@ -19,7 +18,7 @@ int main() {
   string_vector str_vec = {"hijklmop", "a", "abc", "defg"};
   sort_str_vecs(str_vec, str_vec, sort_type::ASC);
 
-  for (const auto str : str_vec) {
+  for (const auto &str : str_vec) {
     std::cout << str << std::endl;
   }
 
