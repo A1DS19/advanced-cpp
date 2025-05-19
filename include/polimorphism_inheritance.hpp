076cb0d62a78ebec13df4ec0fa002c66da3b265e
Jose Enrique Padilla <jp06171@gmail.com>
2025-05-19 00:29:17 -0600

    updates

--- include/polimorphism_inheritance.hpp
+++ include/polimorphism_inheritance.hpp
@@ -5,8 +5,17 @@
 class Test {
 protected:
   std::string string;
+
+public:
+  std::string string2;
+
+private:
+  std::string string3;
 };
 
 class TestOne : public Test {
-  TestOne() { std::cout << string << std::endl; }
+  TestOne() {
+    std::cout << string << std::endl;
+    std::cout << string2 << std::endl;
+  }
 };
